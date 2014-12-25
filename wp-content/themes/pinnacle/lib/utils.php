76a4ba8e01b803c4ac448eeb6ac0bd5540728a52
Jakub Zelenka <bukka@php.net>
2014-12-25 19:25:29 +0000

    Add pinnacle theme

---
+++ wp-content/themes/pinnacle/lib/utils.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Page titles
+ */
+function kadence_title() {
+  if (is_home()) {
+    if (get_option('page_for_posts', true)) {
+      echo get_the_title(get_option('page_for_posts', true));
+    } else {
+      _e('Latest Posts', 'pinnacle');
+    }
+  } elseif (is_archive()) {
+    $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+    if ($term) {
+      echo $term->name;
+    } elseif (is_post_type_archive()) {
+      echo get_queried_object()->labels->name;
+    } elseif (is_day()) {
+      printf(__('Daily Archives: %s', 'pinnacle'), get_the_date());
+    } elseif (is_month()) {
+      printf(__('Monthly Archives: %s', 'pinnacle'), get_the_date('F Y'));
+    } elseif (is_year()) {
+      printf(__('Yearly Archives: %s', 'pinnacle'), get_the_date('Y'));
+    } elseif (is_author()) {
+      printf(__('Author Archives: %s', 'pinnacle'), get_the_author());
+    } else {
+      single_cat_title();
+    }
+  } elseif (is_search()) {
+    printf(__('Search Results for %s', 'pinnacle'), get_search_query());
+  } elseif (is_404()) {
+    _e('Not Found', 'pinnacle');
+  } else {
+    the_title();
+  }
+}
+
+function kad_is_element_empty($element) {
+  $element = trim($element);
+  return empty($element) ? false : true;
+}
---
+++ wp-content/themes/pinnacle/templates/home/callto-action.php
@@ -0,0 +1,16 @@
+<?php global $pinnacle; 
+?>
+<div class="home_calltoaction home-margin clearfix home-padding">
+<div class="kt-home-call-to-action panel-row-style-wide-feature">
+	<div class="container">
+		<div class="kt-cta">
+			<div class="col-md-10 kad-call-title-case">
+				<h1 class="kad-call-title"><?php if(isset($pinnacle['home_action_text'])) echo $pinnacle['home_action_text'];?></h1>
+			</div>
+			<div class="col-md-2 kad-call-button-case">
+			<a href="<?php if(isset($pinnacle['home_action_link'])) echo esc_attr($pinnacle['home_action_link']);?>" class="kad-btn-primary kad-btn lg-kad-btn"><?php if(isset($pinnacle['home_action_text_btn'])) echo $pinnacle['home_action_text_btn'];?></a>   	
+			</div>
+		</div>
+	</div><!--container-->
+</div><!--call class-->
+</div>
