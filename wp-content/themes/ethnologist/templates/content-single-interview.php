4c20b839a8bdb608ca791993e8243fbefe0849e3
Jakub Zelenka <bukka@php.net>
2015-06-29 21:26:39 +0100

    Improve ethnologist interview design

--- wp-content/themes/ethnologist/templates/content-single-interview.php
+++ wp-content/themes/ethnologist/templates/content-single-interview.php
@@ -1,15 +1,12 @@
 <div class="main <?php echo kadence_main_class(); ?>" role="main">
 	<?php while (have_posts()) : the_post(); ?>
 	<article <?php post_class('postclass'); ?>>
-		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php get_template_part('templates/entry', 'meta-subhead'); ?>
-		</header>
 		<div class="entry-content ethnologist-interview-content clearfix">
 			<?php the_content(); ?>
 			<?php the_field( 'interview' ); ?>
 		</div>
 		<footer class="single-footer clearfix">
+			<?php get_template_part('templates/entry', 'meta-subhead'); ?>
 			<?php get_template_part('templates/entry', 'meta-footer'); ?>
 		</footer>
 	</article>
