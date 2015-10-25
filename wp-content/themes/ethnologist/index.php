7fc3b232bcfd0dce6855580a5a00a31f6c2331c1
Jakub Zelenka <bukka@php.net>
2015-10-25 12:47:39 +0000

    Rewrite index.php

---
+++ wp-content/themes/ethnologist/index.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+<?php get_template_part( 'templates/page', 'header' ); ?>
+
+<div id="content" class="container">
+	<div class="row">
+		<div class="main <?php echo ethnologist_main_class(); ?>  postlist" role="main">
+			<?php if ( ! have_posts() ) : ?>
+				<div class="alert">
+					<?php pll_e( 'Sorry, no entries found.' ); ?>
+				</div>
+				<?php get_search_form(); ?>
+			<?php endif; ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'templates/content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+				<nav class="post-nav">
+					<ul class="pager">
+						<li class="previous"><?php next_posts_link( pll__( '&larr; Older posts' ) ); ?></li>
+						<li class="next"><?php previous_posts_link( pll__( 'Newer posts &rarr;' ) ); ?></li>
+					</ul>
+				</nav>
+			<?php endif; ?>
+
+		</div><!-- /.main -->
+		<?php get_sidebar(); ?>
+	</div><!-- /.row-->
+</div><!-- /.content -->
+
+</div><!-- /.wrap -->
+<?php get_footer(); ?>
