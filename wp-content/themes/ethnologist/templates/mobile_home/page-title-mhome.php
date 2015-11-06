2b7c0fdb676484c4bb2ace53499b1cfaf3c801f1
Jakub Zelenka <bukka@php.net>
2015-11-06 19:34:47 +0000

    Rewrite mobile page-title-mhome

---
+++ wp-content/themes/ethnologist/templates/mobile_home/page-title-mhome.php
@@ -0,0 +1,13 @@
+<div id="pageheader" class="titleclass home-title-class">
+<div class="header-color-overlay"></div>
+	<div class="container">
+		<div class="page-header home-page-header">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="kad-page-title home-page-title"><?php pll_e('Ethnologist'); ?></h1>
+					<p class="subtitle home-page-subtitle"><?php pll_e('Welcome to Ethnologist Site'); ?></p>
+				</div>
+			</div>
+		</div>
+	</div><!--container-->
+</div><!--titleclass-->
