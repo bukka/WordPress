10299d7759b3dc55b8b88e827b25f9b7653119be
Jakub Zelenka <bukka@php.net>
2015-11-08 12:32:25 +0000

    Rewrite template for blogs listing

---
+++ wp-content/themes/ethnologist/inc/class-ethnologist-template-blogs.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/class-ethnologist-template-listing.php';
+
+/**
+ * Ethnologist Template Blogs class
+ */
+class Ethnologist_TemplateBlogs extends Ethnologist_TemplateListing
+{
+}
---
+++ wp-content/themes/ethnologist/template-blogs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Ethnologist Blogs
+*/
+
+require_once __DIR__ . '/inc/class-ethnologist-template-blogs.php';
+
+$template = new Ethnologist_TemplateBlogs();
+$template->render();
