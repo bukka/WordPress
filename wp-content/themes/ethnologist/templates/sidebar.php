435a2d86bd4ad1c4a006199cdfd9d32c976ef90d
Jakub Zelenka <bukka@php.net>
2016-08-29 16:54:52 +0100

    Add tag cloud to sidebar

--- wp-content/themes/ethnologist/templates/sidebar.php
+++ wp-content/themes/ethnologist/templates/sidebar.php
@@ -2,6 +2,7 @@
 	<aside class="<?php echo ethnologist_sidebar_class(); ?>" role="complementary">
 		<div class="sidebar">
 			<?php dynamic_sidebar( 'sidebar-primary' ); ?>
+			<?php wp_tag_cloud(['taxonomy' => 'category']); ?>
 		</div><!-- /.sidebar -->
 	</aside><!-- /aside -->
 <?php endif; ?>
