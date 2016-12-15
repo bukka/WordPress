ccbe193a0c9967cb3e28dbe3e1cf09ec39112d8a
Jakub Zelenka <bukka@php.net>
2016-12-15 13:53:13 +0000

    Add title and style post title in editors pick

--- wp-content/themes/ethnologist/views/widget/editors-pick-form.php
+++ wp-content/themes/ethnologist/views/widget/editors-pick-form.php
@@ -1,4 +1,10 @@
 <p>
+	<label for="<?php echo esc_attr( $context->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:', 'ethnologist' ); ?></label>
+	<input class="widefat" id="<?php echo esc_attr( $context->get_field_id( 'title' ) ); ?>"
+		name="<?php echo esc_attr( $context->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $params['title'] ); ?>" />
+</p>
+
+<p>
 	<label for="<?php echo $context->get_field_id( 'post_id' ); ?>">
 		<?php _e( 'Post:', 'ethnologist' ); ?>
 	</label>
