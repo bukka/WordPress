3458f466f6903098a947cc458d8a4ba1a8437632
Jakub Zelenka <bukka@php.net>
2017-03-26 20:33:27 +0100

    Add db-error page

---
+++ wp-content/db-error.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>The Ethnologist website maintenance</title>
+		<style type="text/css">
+		h1 { margin: 20px auto; text-align: center; color: #777; }
+		</style>
+	</head>
+	<body>
+		<h1>The Ethnologist website is currently under maintenance.</h1>
+	</body>
+</html>
