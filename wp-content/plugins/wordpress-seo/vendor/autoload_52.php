2ffc73589885a6a110ae3eccdc7a897197ffee2b
Jakub Zelenka <bukka@php.net>
2018-09-02 20:40:55 +0100

    Update Yoast SEO to 8.1

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit63dcc7aa5f4f5ecf72f96589dda1ae3d::getLoader();
+return ComposerAutoloaderInitfbe5b803497ba919f3726be310778fcd::getLoader();
