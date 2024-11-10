b5ae4ae54af94aa0eca4bb67b1378451cd034857
Jakub Zelenka <bukka@php.net>
2024-11-10 17:05:07 +0100

    Update Polylang to 3.6.5

--- wp-content/plugins/polylang/vendor/composer/installed.php
+++ wp-content/plugins/polylang/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpsyntex/polylang',
         'pretty_version' => '3.6.x-dev',
         'version' => '3.6.9999999.9999999-dev',
-        'reference' => 'd762db6d83067556f6270aa0f41b8e0b9b427477',
+        'reference' => 'a3b0bdc67851fdf91f76ad00d4eb7f8bcaf5d87b',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'wpsyntex/polylang' => array(
             'pretty_version' => '3.6.x-dev',
             'version' => '3.6.9999999.9999999-dev',
-            'reference' => 'd762db6d83067556f6270aa0f41b8e0b9b427477',
+            'reference' => 'a3b0bdc67851fdf91f76ad00d4eb7f8bcaf5d87b',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
